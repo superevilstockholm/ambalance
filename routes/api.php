69b8f70591f8378260604602650f28ecd8cb3624
superevilstockholm <balilombok11@gmail.com>
2025-10-13 21:23:42 +0700

    Created login API

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+// Auth
+use App\Http\Controllers\AuthController;
+
+Route::post('/login', [AuthController::class, 'login']);
 
 // Protected routes
 Route::middleware(['auth:sanctum'])->group(function () {
