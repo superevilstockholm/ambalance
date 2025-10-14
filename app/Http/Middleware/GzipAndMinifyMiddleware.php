4abdc13374e4907116a8b57ecf0e841723560b15
superevilstockholm <balilombok11@gmail.com>
2025-10-14 09:24:17 +0700

    Created Gzip and HTML Minify middleware, created required routes, and register all middleware

---
+++ app/Http/Middleware/GzipAndMinifyMiddleware.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class GzipAndMinifyMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        if ($response instanceof Response && str_contains($response->headers->get('Content-Type', ''), 'text/html')) {
+            $content = $response->getContent();
+
+            // Minify
+            $content = preg_replace([
+                '/\s{2,}/', // 2 or more spaces
+                '/\n|\r/', // new line
+                '/<!--.*?-->/', // HTML comments
+                '/\/\*.*?\*\//' // CSS comments
+            ], [' ', '', '', ''], $content);
+
+            // Gzip
+            if (isset($_SERVER['HTTP_ACCEPT_ENCODING']) && strpos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') !== false) {
+                $content = gzencode($content, 9);
+                $response->headers->set('Content-Encoding', 'gzip');
+            }
+
+            $response->setContent($content);
+            $response->headers->set('Content-Length', strlen($content));
+        }
+
+        return $response;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,77 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('pages.index');
 })->name('index');
+Route::get('/about', function () {
+
+})->name('about');
+
+// Optional protected routes
+Route::middleware(['optional.auth.sanctum.cookie'])->group(function () {
+    Route::get('/login', function (Request $request) {
+        if ($request->user()) {
+            return redirect(route($request->user()->role . '.dashboard'));
+        }
+        
+    })->name('login');
+    Route::get('/register', function (Request $request) {
+        if ($request->user()) {
+            return redirect(route($request->user()->role . '.dashboard'));
+        }
+
+    })->name('register');
+});
+
+// Protected routes
+Route::middleware(['auth.sanctum.cookie'])->group(function () {
+    Route::prefix('dashboard')->group(function () {
+        // Protectetd - Role: Student
+        Route::middleware(['role:student'])->group(function () {
+            Route::prefix('student')->group(function () {
+                Route::get('/', function () {
+
+                })->name('student.dashboard');
+                Route::get('/savings-history', function () {
+
+                })->name('student.savings-history');
+            });
+        });
+        // Protectetd - Role: Teacher
+        Route::middleware(['role:teacher'])->group(function () {
+            Route::prefix('teacher')->group(function () {
+                Route::get('/', function () {
+
+                })->name('teacher.dashboard');
+                Route::get('/students', function () {
+
+                })->name('teacher.students');
+            });
+        });
+        // Protectetd - Role: Admin
+        Route::middleware(['role:admin'])->group(function () {
+            Route::prefix('admin')->group(function () {
+                Route::get('/', function () {
+
+                })->name('admin.dashboard');
+                Route::prefix('master-data')->group(function () {
+                    Route::get('/classes', function () {
+
+                    })->name('admin.master-data.classes');
+                    Route::get('/students', function () {
+
+                    })->name('admin.master-data.students');
+                    Route::get('/teachers', function () {
+
+                    })->name('admin.master-data.teachers');
+                    Route::get('/savings', function () {
+
+                    })->name('admin.master-data.savings');
+                });
+            });
+        });
+    });
+});
