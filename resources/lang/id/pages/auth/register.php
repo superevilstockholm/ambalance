561c7cc4e0bee21e9af5cfee0323462d0a842155
superevilstockholm <balilombok11@gmail.com>
2025-11-14 09:58:56 +0700

    Created translate id and en in register page

---
+++ resources/lang/id/pages/auth/register.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    // Title
+    'welcome_aboard' => 'Selamat Datang!',
+    'student_message' => 'Daftar ke :webname_link dengan informasi siswa kamu',
+    'teacher_message' => 'Daftar ke :webname_link dengan informasi guru kamu',
+    'webname_message' => 'Ambalance',
+
+    // Fields
+    'nisn' => 'NISN',
+    'nip' => 'NIP',
+    'dob' => 'Tanggal Lahir',
+    'email' => 'Email',
+    'password' => 'Kata Sandi',
+    'confirm_password' => 'Konfirmasi Kata Sandi',
+
+    // Buttons
+    'sign_up' => 'Daftar',
+
+    // Login
+    'already_have_an_account_message' => 'Sudah punya akun? :login_link',
+    'login_message' => 'Masuk di sini',
+
+    // Teacher Register
+    'you_are_a_teacher_message' => 'Kamu seorang guru? :teacher_register_button',
+    'teacher_register_message' => 'Daftar di sini',
+
+    // Student Login
+    'you_are_a_student_message' => 'Kamu seorang siswa? :student_register_button',
+    'student_register_message' => 'Daftar di sini',
+];
