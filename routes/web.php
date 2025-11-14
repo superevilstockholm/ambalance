699fb5bdfca6cc4dc3f40bacd1424b12cb23475f
superevilstockholm <balilombok11@gmail.com>
2025-11-14 10:20:16 +0700

    Created change locale language route and middleware

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
+
+Route::get('lang/{locale}', function ($locale) {
+    if (!in_array($locale, ['en', 'id'])) { abort(400); }
+    App::setLocale($locale);
+    Session::put('locale', $locale);
+    return redirect()->back();
+})->name('switch-lang');
+
 Route::get('/', function () {
     return view('pages.index');
 })->name('index');
